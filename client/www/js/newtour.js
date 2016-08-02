f1fbb3bfd54fcd44df4c6c67925aa75f2ecd8042
Sohil Bammi <sohilbammi@gmail.com>
2016-08-02 11:58:23 -0700

    New Tour Screen & Create Tour Function

--- client/www/js/newtour.js
+++ client/www/js/newtour.js
@@ -6,5 +6,19 @@ angular.module('starter')
         var currUser = Filuser.getCachedCurrent();
         console.log(currUser.id);
 
+        $scope.createTour = function () {
+            $scope.tourdetails.startTime = new Date().toJSON();
+            $scope.tourdetails.endTime = new Date().toJSON();
+            $scope.tourdetails.tourguideId = currUser.id;
+            var tour = Tour.create($scope.tourdetails)
+                .$promise
+                .then(function (res) {
+                    console.log('Tour Created: '+ tour);
+                    $location.path('/tourguide-tabs/tourguide/profile');
+                }, function (err) {
+                    console.log('Error inside Create Tour Function');
+                    $scope.tourError = err;
+                });
+        };
         
     });
